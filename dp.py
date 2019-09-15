7e860314fe13c40167101d975762d4f29be93caa
ramizraza509 <55353708+ramizraza509@users.noreply.github.com>
2019-09-15 23:30:44 +0530

    Delete dp.py

--- dp.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector
-mydb=mysql.connector.connect(host="localhost",user="root",password="")
-mycursor=mydb.cursor()
-mycursor.execute("CREATE TABLE customers1 (name VARCHAR(255), address VARCHAR(255))")
-#mycursor.execute("""INSERT INT customers VALUES'ramiz','Chaibasa')""")
